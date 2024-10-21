38f745a6dad6a23275592909af60748f3144615b
Zemaoh <jackvald20@gmail.com>
2024-10-21 16:09:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const app = express();
 
-// Set EJS as the templating engine
+
 app.set('view engine', 'ejs');
 
-// Middleware to serve static files
+
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-// Blog posts storage (in-memory)
+
 let posts = [];
 
-// Route to home page
+
 app.get('/', (req, res) => {
     res.render('index', { posts });
 });
 
-// Route to create a new post (GET)
+
 app.get('/new', (req, res) => {
     res.render('new');
 });
 
-// Route to handle new post submission (POST)
+
 app.post('/new', (req, res) => {
     const { title, content } = req.body;
     if (title && content) {
@@ -30,7 +30,7 @@ app.post('/new', (req, res) => {
     res.redirect('/');
 });
 
-// Start the server
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
